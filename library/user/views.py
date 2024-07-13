94a1f663fba8945acc0038b621f5536ee330bb01
Jan Romanowski <50454481+Jan-Romanowski@users.noreply.github.com>
2024-07-13 09:27:12 +0200

    asdasd

--- library/user/views.py
+++ library/user/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm  # убедитесь, что этот импорт добавлен
 
 def index(request):
     return render(request, 'user/index.html')
-
 
 def signIn(request):
     return render(request, 'user/signIn.html')
 
 def signUp(request):
-    return render(request, 'user/signUp.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')  # Перенаправление на главную страницу
+    else:
+        form = SignUpForm()
+    return render(request, 'user/signUp.html', {'form': form})
