6cbd9f4370f849f6c803f82ba569e801e8324368
Jan Romanowski <50454481+Jan-Romanowski@users.noreply.github.com>
2024-07-15 15:13:50 +0200

    Temp changes

--- library/user/forms.py
+++ library/user/forms.py
@@ -1,25 +1,36 @@
 from .models import Users
-from django.forms import ModelForm, TextInput
-
+from django.forms import ModelForm, TextInput, EmailInput, PasswordInput
 
 class UsersForm(ModelForm):
     class Meta:
         model = Users
         fields = ['email', 'name', 'surname', 'password']
 
-        widgets = {
-            "title": TextInput(attrs={
-                'class': '',
-                'placeholder': ''
-            })
+        labels = {
+            'email': 'Email',
+            'name': 'Imię',
+            'surname': 'Nazwisko',
+            'password': 'Hasło',
         }
 
-        def clean_email(self):
-            email = self.cleaned_data.get('email')
-            # Проверка на уникальность email
-            if Users.objects.filter(email=email).exists():
-                raise forms.ValidationError('Этот email уже используется.')
-            return email
+        widgets = {
+            'email': EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'example@example.com'
+            }),
+            'name': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Wpisz imię'
+            }),
+            'surname': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Wpisz nazwisko'
+            }),
+            'password': PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Haslo123'
+            }),
+        }
 
         # name = models.CharField('name', max_length=25)
         # surname = models.CharField('surname', max_length=25)
