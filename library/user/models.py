1ed120916a3228a39005a90c07d63a10e68f9493
Jan Romanowski <50454481+Jan-Romanowski@users.noreply.github.com>
2024-07-26 10:14:42 +0200

    Fix registration

--- library/user/models.py
+++ library/user/models.py
@@ -22,7 +22,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
 
