889c081a79d1fbf5a1c2b268a01bdfec1c0899d7
Jan Romanowski <50454481+Jan-Romanowski@users.noreply.github.com>
2024-09-21 15:29:54 +0200

    tmp changes

--- library/composition/templatetags/extras.py
+++ library/composition/templatetags/extras.py
@@ -1,5 +1,7 @@
 # composition/templatetags/composition_extras.py
 from django import template
+from django.utils import timezone
+from datetime import timedelta
 
 register = template.Library()
 
@@ -18,3 +20,16 @@ def get_filename_without_extension(path):
     """
     filename = path.rsplit('/', 1)[-1] if '/' in path else path
     return filename.split('.')[0] if '.' in filename else filename
+
+@register.filter
+def activity_status(last_activity):
+    if last_activity is None:
+        return "offline"
+    
+    now = timezone.now()
+    time_diff = now - last_activity
+
+    if time_diff < timedelta(minutes=5):
+        return "online"
+    else:
+        return f"{time_diff.seconds // 60} minut temu"
--- library/user/templatetags/__init__.py
+++
@@ -1,1 +0,0 @@
-# __init__.py в папке templatetags
